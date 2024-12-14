d7bcd0034093cce60b49e1eebecd00ef44fbd80d
MasakiCodes <masaki.horikawa@gmail.com>
2024-12-14 23:18:03 +1100

    mobile responsiveness in the navigation bar

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ document.getElementById('search').addEventListener('input', function(e) {
         }
     });
 });
+
+const menuToggle = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
