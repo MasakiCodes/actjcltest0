d622e2e88e00013b6e72500bfff613e0d5fedf8e
MasakiCodes <masaki.horikawa@gmail.com>
2024-12-31 13:29:40 +1100

    new About Us page with a unmade About Us page. With better filing and parent folders.

--- js/script.js
+++ js/script.js
@@ -28,9 +28,3 @@ document.getElementById('search-completed').addEventListener('input', function (
     });
 });
 
-const menuToggle = document.getElementById('mobile-menu');
-const navLinks = document.querySelector('.nav-links');
-
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
